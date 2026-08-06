18aeb8e9e8124485ed57e078df996eae53ada51b
Max Schwesig <m.schwesig@schlumberger.digital>
2026-08-06 12:09:09 +0200

    Diverses zum Buil

---
+++ Uploaddocu.bat
@@ -0,0 +1,18 @@
+@REM """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+@REM  Windows Batch Skript - lädt doku hoch
+@REM 			   
+@REM 
+@REM  Erstellt von:	 Max Schwesig  
+@REM  version = "3.0"
+
+@REM  Codierung:    UTF-8	
+@REM  Stand:	06.08.2026 Max Schwesig	
+ 
+@REM  Copyright (c) 2026 schlumberger digital. Alle Rechte vorbehalten.
+@REM  www.schlumberger.digital
+@REM \*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
+@echo off
+set PYTHONIOENCODING=utf-8
+pushd %~dp0
+zensical build
+python upload_docu.py
